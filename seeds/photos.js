0c71e2d8086d15ca4b08401e24bd5b6c1d3ff501
Andrew Wadman <email": null>
2016-05-11 12:32:24 +1200

    added seeds

--- seeds/photos.js
+++ seeds/photos.js
@@ -7,8 +7,11 @@ exports.seed = function(knex, Promise) {
     // Inserts seed entries
     knex('photos').insert({userId: 1, countryId: 233, cityId: 1, caption: 'I am riding a horse', rating: 100, link: "https://s-media-cache-ak0.pinimg.com/236x/13/4a/78/134a78460defef0bc46ef4bd2174e7f0.jpg", categoryId: 7, created_at: "1999-01-08 04:05:06", updated_at: "1999-01-08 04:05:06"}),
     knex('photos').insert({userId: 2, countryId: 171, cityId: 2, caption: 'Look an albino kiwi!', rating: -56, link: "https://s-media-cache-ak0.pinimg.com/236x/23/34/c1/2334c175f848b17e62cda6d8d4c8ad8b.jpg", categoryId: 1, created_at: "1999-01-08 04:05:06", updated_at: "1999-01-08 04:05:06"}),
-    knex('photos').insert({userId: 3, countryId: 48, cityId: 3, caption: 'Just read the docs', rating: 45, link: "http://i.dailymail.co.uk/i/pix/2013/05/09/article-0-19B357D8000005DC-348_634x607.jpg", categoryId: 8, created_at: "1999-01-08 04:05:06", updated_at: "1999-01-08 04:05:06"}),
-    knex('photos').insert({userId: 4, countryId: 171, cityId: 4, caption: 'Im cool', rating: 45, link: "https://s-media-cache-ak0.pinimg.com/736x/6a/f7/36/6af736428d0934c38636953a94988f25.jpg", categoryId: 8, created_at: "1999-01-08 04:05:06", updated_at: "1999-01-08 04:05:06"})
-
+    knex('photos').insert({userId: 3, countryId: 48, cityId: 3, caption: 'Just read the docs', rating: 45, link: "https://parisiangentleman.fr/wp-content/uploads/2014/05/Mao_Zedong_pr%C3%A9sident.jpg", categoryId: 8, created_at: "1999-01-08 04:05:06", updated_at: "1999-01-08 04:05:06"}),
+    knex('photos').insert({userId: 4, countryId: 171, cityId: 4, caption: 'Im cool', rating: 45, link: "https://s-media-cache-ak0.pinimg.com/736x/6a/f7/36/6af736428d0934c38636953a94988f25.jpg", categoryId: 8, created_at: "1999-01-08 04:05:06", updated_at: "1999-01-08 04:05:06"}),
+    knex('photos').insert({userId: 5, countryId: 171, cityId: 4, caption: 'Im Mix', rating: 76, link: "http://b-i.forbesimg.com/eliseackerman/files/2013/07/300px-Linus_Torvalds_flipped4.jpg", categoryId: 7, created_at: "1999-01-08 04:05:06", updated_at: "1999-01-08 04:05:06"}),
+    knex('photos').insert({userId: 5, countryId: 171, cityId: 4, caption: 'Im Mix', rating: 76, link: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Linus_Torvalds_talking.jpeg", categoryId: 7, created_at: "1999-01-08 04:05:06", updated_at: "1999-01-08 04:05:06"}),
+    knex('photos').insert({userId: 2, countryId: 171, cityId: 4, caption: 'Im Sam', rating: 76, link: "https://static.pexels.com/photos/6980/summer-cute-hipster-beauty.jpg", categoryId: 1, created_at: "1999-01-08 04:05:06", updated_at: "1999-01-08 04:05:06"}),
+    knex('photos').insert({userId: 2, countryId: 171, cityId: 4, caption: 'Im Sam', rating: 76, link: "http://media2.popsugar-assets.com/files/2014/04/14/605/n/1922564/99c1a707a532838a_484550455.xxxlarge_2x/i/Kate-Middleton-New-Zealand.jpg", categoryId: 4, created_at: "1999-01-08 04:05:06", updated_at: "1999-01-08 04:05:06"})
   );
 };
--- seeds/users.js
+++ seeds/users.js
@@ -11,6 +11,7 @@ exports.seed = function(knex, Promise) {
     knex('users').insert({fullName: 'Andrew Wadman', profilePicture: "https://s-media-cache-ak0.pinimg.com/236x/13/4a/78/134a78460defef0bc46ef4bd2174e7f0.jpg", passwordHash: bcrypt.hashSync('password', saltRounds), email: 'andrewwadman@gmail.com', styleRating: 1, connoisseurRating: 1, fbId: 1, igId: 1}),
     knex('users').insert({fullName: 'Sam Simmons', profilePicture: "https://s-media-cache-ak0.pinimg.com/236x/13/4a/78/134a78460defef0bc46ef4bd2174e7f0.jpg", passwordHash: bcrypt.hashSync('password', saltRounds), email: 'samsimmons@gmail.com', styleRating: 2, connoisseurRating: 2, fbId: 2, igId: 2}),
     knex('users').insert({fullName: 'Vicken Liu', profilePicture: "https://s-media-cache-ak0.pinimg.com/236x/13/4a/78/134a78460defef0bc46ef4bd2174e7f0.jpg", passwordHash: bcrypt.hashSync('password', saltRounds), email: 'vickenliu@gmail.com', styleRating: 3, connoisseurRating: 3, fbId: 3, igId: 3}),
-    knex('users').insert({fullName: 'Tony Luisi' , passwordHash: bcrypt.hashSync('password', saltRounds), email: 'tluisi@gmail.com', styleRating: 3, connoisseurRating: 3, fbId: 10156915534565451, igId: 3})
+    knex('users').insert({fullName: 'Tony Luisi', passwordHash: bcrypt.hashSync('password', saltRounds), email: 'tluisi@gmail.com', styleRating: 3, connoisseurRating: 3, fbId: 10156915534565451, igId: 4}),
+    knex('users').insert({fullName: 'Mix Irving', passwordHash: bcrypt.hashSync('password', saltRounds), email: 'mix@gmail.com', styleRating: 3, connoisseurRating: 3, fbId: 10156915534565452, igId: 5})
   );
 };
