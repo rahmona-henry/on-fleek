970821f994a64903319df749f115d7077a0fac46
tony-luisi <t.luisi@gmail.com>
2016-06-01 14:25:22 +1200

    linted

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 var path = require('path');
-var db = require('./database/db');
-//express
+
+// express
 var express = require('express');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -9,24 +9,23 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session')
 
-//knex
+// knex
 var Knex = require('knex');
-var knexConfig = require(__dirname + '/knexfile');
+var knexConfig = require(path.join(__dirname, '/knexfile'));
 var knex = Knex(knexConfig[process.env.NODE_ENV || 'development']);
 var db = require('./database/db')
 
-//passport
+// passport
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook');
 var InstagramStrategy = require('passport-instagram');
 
-//routes
+// routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var auth = require('./routes/auth');
 var test = require('./routes/test');
 var app = express();
-var db = require('./database/db');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -39,75 +38,73 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//session initialisation
+// session initialisation
 app.use(session({
   secret: 'This is a secret!',
   saveUninitialized: true,
   resave: true,
   db: knex,
-  cookie: {maxAge: 24*60*60*1000}
+  cookie: {maxAge: 24 * 60 * 60 * 1000}
 }))
 
-//Passport initialisation
+// Passport initialisation
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Facebook strategy
 passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_APP_ID,
-    clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: process.env.DOMAIN + "/auth/facebook/callback/",
-    profileFields: ['id', 'displayName', 'email']
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    var user = profile
-    var checkUser = {}
-    checkUser.fullName = user.displayName
-    checkUser.email = user.emails[0].value
-    checkUser.fbId = user.id
-    db.findOrCreate(checkUser, function(returnedUser){
-      user.dbid = returnedUser.id
-      return cb(null, user)
-    })
-  }
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: process.env.DOMAIN + '/auth/facebook/callback/',
+  profileFields: ['id', 'displayName', 'email']
+},
+function (accessToken, refreshToken, profile, cb) {
+  var user = profile
+  var checkUser = {}
+  checkUser.fullName = user.displayName
+  checkUser.email = user.emails[0].value
+  checkUser.fbId = user.id
+  db.findOrCreate(checkUser, function (returnedUser) {
+    user.dbid = returnedUser.id
+    return cb(null, user)
+  })
+}
 ));
 
 // Instagram strategy
 passport.use(new InstagramStrategy({
-    clientID: process.env.INSTAGRAM_CLIENT_ID,
-    clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
-    callbackURL: process.env.DOMAIN + "/auth/instagram/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    var user = profile
-    var checkUser = {}
-    checkUser.fullName = user.displayName
-    checkUser.igId = user.id
-    db.findOrCreate(checkUser, function(returnedUser){
-      user.dbid = returnedUser.id
-      return cb(null, user)
-    })
-  }
+  clientID: process.env.INSTAGRAM_CLIENT_ID,
+  clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
+  callbackURL: process.env.DOMAIN + '/auth/instagram/callback'
+},
+function (accessToken, refreshToken, profile, cb) {
+  var user = profile
+  var checkUser = {}
+  checkUser.fullName = user.displayName
+  checkUser.igId = user.id
+  db.findOrCreate(checkUser, function (returnedUser) {
+    user.dbid = returnedUser.id
+    return cb(null, user)
+  })
+}
 ));
 
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
   cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
+passport.deserializeUser(function (obj, cb) {
   cb(null, obj);
 });
 
-//routing
+// routing
 app.use('/', routes);
 app.use('/users', users);
 app.use('/auth', auth)
 app.use('/test', test)
 
-app.get('*', function (request, response){
+app.get('*', function (request, response) {
   response.sendFile(path.resolve(__dirname, './public', 'index.html'))
 })
-
-
 
 module.exports = app;
